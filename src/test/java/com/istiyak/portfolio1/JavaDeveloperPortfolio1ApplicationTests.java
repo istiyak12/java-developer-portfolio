10fa9294b1c1e169634fd8d3c45c99e79340ce18
Istiyak Ahamad <istiyakahamad570@gmail.com>
2026-09-16 15:31:31 +0530

    Initial commit

---
+++ src/test/java/com/istiyak/portfolio1/JavaDeveloperPortfolio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istiyak.portfolio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDeveloperPortfolio1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
