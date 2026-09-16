ef070ec155b50ba3b42139d5fa8a4ba48b3dbe05
Istiyak Ahamad <istiyakahamad570@gmail.com>
2026-09-16 15:31:31 +0530

    Update GitHub profile link

---
+++ src/main/java/com/istiyak/portfolio1/JavaDeveloperPortfolio1Application.java
@@ -0,0 +1,13 @@
+package com.istiyak.portfolio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDeveloperPortfolio1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDeveloperPortfolio1Application.class, args);
+    }
+
+}
